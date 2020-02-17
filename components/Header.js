455ea09085fdd5020837e1a90b2eb1f90cb1d7fa
Devin Halladay <devin@devinhalladay.com>
2020-02-16 23:35:06 -0500

    Header css mostly

--- components/Header.js
+++ components/Header.js
@@ -3,23 +3,38 @@ import Panel from './Panel'
 // import { withRouter , BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 
 const Header = props => {
-  let isAuthenticated = false
+  let isAuthenticated = true
 
-  return (
-    <Panel pinSide="center">
-      <header>
-        <nav>
-          <ul>
-            <li>
+  if (isAuthenticated) {
+    return (
+      <Panel pinSide="center">
+        <header>
+          <nav>
+            <div className="orchard-title"><p>https://www.are.na/<strong>devin-halladay/</strong></p></div>
+            <form action="#" className="grove-navigation">
+              <input type="text" placeholder="Enter channel name…"/>
+            </form>
+          </nav>
+        </header>
+      </Panel>
+    )
+  } else {
+    return (
+      <Panel pinSide="center">
+        <header>
+          <nav>
+            <ul>
+              <li>
                 <a>Home</a>
-            </li>
-            {isAuthenticated ? <li><a href="/orchard">Orchard</a></li> : null}
-            {!isAuthenticated ? <li><a href={`https://dev.are.na/oauth/authorize?client_id=${process.env.REACT_APP_APPLICATION_ID}&redirect_uri=https://localhost:3000/oauth/callback&response_type=code`}>Login</a></li> : null}
-          </ul>
-        </nav>
-      </header>
-    </Panel>
-  );
+              </li>
+              {isAuthenticated ? <li><a href="/orchard">Orchard</a></li> : null}
+              {!isAuthenticated ? <li><a href={`https://dev.are.na/oauth/authorize?client_id=${process.env.REACT_APP_APPLICATION_ID}&redirect_uri=https://localhost:3000/oauth/callback&response_type=code`}>Login</a></li> : null}
+            </ul>
+          </nav>
+        </header>
+      </Panel>
+    );
+  }
 }
 
 export default Header
