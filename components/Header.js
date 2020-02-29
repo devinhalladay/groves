fffbf27130fe8185aa6ea2614ffcc47beae2d138
Devin Halladay <devin@devinhalladay.com>
2020-02-28 21:25:17 -0500

    Move arena-client to utils

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ const Header = ({setIsReady, user, setSelectedChannel, selectedChannel, channels
         <header>
           <nav>
             <div className="orchard-title">
-              <a href={`https://dev.are.na/oauth/authorize?client_id=${process.env.APPLICATION_ID}&redirect_uri=${process.env.APPLICATION_CALLBACK}&response_type=code`}>https://www.are.na/<strong>devin-halladay/</strong></a>
+              <a href={`https://dev.are.na/oauth/authorize?client_id=${process.env.APPLICATION_ID}&redirect_uri=${process.env.APPLICATION_CALLBACK}&response_type=code`}><span className="gray">https://www.are.na/</span><strong>devin-halladay/</strong></a>
               <div className="icon icon--caret-down">
                 <svg width="11" height="6" viewBox="0 0 11 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M5.14645 5.14645L0.853553 0.853553C0.538571 0.538571 0.761654 0 1.20711 0H9.79289C10.2383 0 10.4614 0.538572 10.1464 0.853554L5.85355 5.14645C5.65829 5.34171 5.34171 5.34171 5.14645 5.14645Z" fill="#333333"/>
--- utils/arena-client.js
+++ utils/arena-client.js
@@ -9,6 +9,7 @@ export default class ArenaClient {
       });
     }
 
+    // Make an API request to Are.na, returning the axios promise.
     _makeRequest(method, path, body = {}) {
       return this.ArenaQuery[method](`${path}`, {
         body: body
@@ -28,14 +29,52 @@ export default class ArenaClient {
     }
 
     getChannelsForMe() {
-      return this._makeRequest('get', `/users/${this.me.id}/channels`).then(resp => {
-        // if (condition) {
-          
-        // } else {
-          
-        // }
+      let channelsArray = []
+      let amountPerPage = 100;
+      let initialPage = 1;
 
-        return resp.data
-      });
+      return new Promise(async (resolve, reject) => {
+        await this._makeRequest('get', `/users/${this.me.id}/channels?per=${amountPerPage}&page=${initialPage}`).then(async resp => {
+          if (resp.data.total_pages === 1) {
+            resolve(resp.data.channels);
+          } else {
+            channelsArray.push(...resp.data.channels);          
+  
+            for (let i = initialPage + 1; i <= resp.data.total_pages; i++) {
+              await this._makeRequest('get', `/users/${this.me.id}/channels?per=${amountPerPage}&page=${i}`).then(resp => {
+                channelsArray.push(...resp.data.channels);
+              })
+            }
+  
+            resolve(channelsArray);
+          }
+        });
+      })
+    }
+
+    async getBlocksFromChannel(channelID, channelLength) {
+      let blocksArray = [];
+      let amountPerPage = 100;
+      let initialPage = 1;      
+
+      return new Promise(async (resolve, reject) => {
+        await this._makeRequest('get', `/channels/${channelID}/contents?per=${amountPerPage}&page=${initialPage}`).then(async resp => {
+          let pageCount = Math.ceil(channelLength / amountPerPage);
+
+          if (pageCount === 1) {
+            resolve(resp.data.contents)
+          } else {
+            blocksArray.push(...resp.data.contents)
+
+            for (let i = initialPage + 1; i <= pageCount; i++) {
+              await this._makeRequest('get', `/channels/${channelID}/contents?per=${amountPerPage}&page=${i}`).then(resp => {
+                blocksArray.push(...resp.data.contents)
+              })
+            }
+
+            resolve(blocksArray)
+          }
+        })
+      })
     }
   }
