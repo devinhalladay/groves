62088ae17e475786a13ff62db773412374f40502
Devin Halladay <devin@devinhalladay.com>
2020-06-18 20:25:40 -0400

    Fix graphql authorization headers

--- pages/api/apollo-client.js
+++ pages/api/apollo-client.js
@@ -10,25 +10,18 @@ export default async (req, res) => {
   
   const authURL = `https://api.are.na/graphql`
 
-  console.log(req.headers['authorization']);
-  console.log(process.env.GRAPHQL_TOKEN);
-  
   try {
     await axios.post(authURL, {
       query
     }, {
       headers: {
-        ...req.headers['authorization'],
+        "authorization": `${req.headers['authorization']}`,
         "X-APP-TOKEN": process.env.GRAPHQL_TOKEN,
       }
     }).then(response => {
-      console.log(response.data);
-      
       res.status(response.status).json(response.data)
     })
   } catch (error) {
-    console.log(error);
-    
     return error
     res.status(400).json({ message: error })
   }
