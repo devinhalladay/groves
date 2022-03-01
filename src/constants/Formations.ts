ea54ceb52936824f50512b4ec1e0818127f23678
Devin Halladay <devin@devinhalladay.com>
2022-02-28 19:51:50 -0500

    Format files

--- src/constants/Formations.ts
+++ src/constants/Formations.ts
@@ -17,4 +17,4 @@ export default {
     title: 'Channel Index',
     icon: 'list-detail-view',
   },
-}
+};
--- src/constants/KeyMaps.ts
+++ src/constants/KeyMaps.ts
@@ -2,36 +2,36 @@ export default {
   VIEW_SHORTCUTS: {
     name: 'View keyboard shortcuts',
     sequence: 'shift+?',
-    group: 'Global Shortcuts'
+    group: 'Global Shortcuts',
   },
   VIEW_GRID: {
     name: 'Grid view',
     sequence: 'w 1',
-    group: 'Global Shortcuts'
+    group: 'Global Shortcuts',
   },
   VIEW_CANVAS: {
     name: 'Canvas view',
     sequence: 'w 2',
-    group: 'Global Shortcuts'
+    group: 'Global Shortcuts',
   },
   VIEW_CHANNEL_INDEX: {
     name: 'Channel Index view',
     sequence: 'w 3',
-    group: 'Global Shortcuts'
+    group: 'Global Shortcuts',
   },
   VIEW_FOLDERS: {
     name: 'Folder View',
     sequence: 'w 4',
-    group: 'Global Shortcuts'
+    group: 'Global Shortcuts',
   },
   FOCUS_NAVIGATOR: {
     name: 'Focus navigator input',
     sequence: 'command+u',
-    group: 'Global Shortcuts'
+    group: 'Global Shortcuts',
   },
   SWITCH_THEME: {
     name: 'Change theme',
     sequence: 'ctrl+d',
-    group: 'Global Shortcuts'
-  }
+    group: 'Global Shortcuts',
+  },
 };
