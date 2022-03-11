da754bbbe375c65a28cba6f114585699f6bc4337
Devin Halladay <devin@devinhalladay.com>
2022-03-11 02:03:41 -0500

    first pass at swapping downshift for blueprint suggest

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -130,7 +130,7 @@ export default withApollo(({ blocks }: ICanvas) => {
         },
         mousewheel: {
           enabled: true,
-          modifiers: 'meta',
+          modifiers: 'cmd',
         },
         resizing: {
           enabled: true,
