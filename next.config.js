c9cbfbbe293f006d377478a9cc5b90f73685451a
Devin Halladay <devin@devinhalladay.com>
2022-06-29 11:31:00 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,11 +12,4 @@ module.exports = withYaml({
     AUTHENTICATION_ENABLED: process.env.NODE_ENV === 'production' ? false : true
   }
 
-  // webpack: function (config) {
-  //   config.module.rules.push({
-  //     test: /\.ya?ml$/,
-  //     use: 'js-yaml-loader'
-  //   });
-  //   return config;
-  // }
 });
